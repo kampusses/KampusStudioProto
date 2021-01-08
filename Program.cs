46ebf2601daf5f975a729ae65b0adb1b2984f068
Vincenzo Camporeale <vincenzo.camporeale@gmail.com>
2021-01-08 22:58:04 +0100

    Aggiornato a .NET 3.0, risolte breaking change e installato EFCore

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace KampusStudioProto
@@ -17,8 +18,10 @@ namespace KampusStudioProto
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHostBuider => {
+                    webHostBuider.UseStartup<Startup>();
+                });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace KampusStudioProto
 {
@@ -18,7 +19,12 @@ namespace KampusStudioProto
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                             #if DEBUG
+                             .AddRazorRuntimeCompilation()
+                             #endif
+                             ;
+
             services.AddTransient<IComuneService, AdoNetComuneService>();
             services.AddTransient<IProvinciaService, AdoNetProvinciaService>();
             services.AddTransient<IRegioneService, AdoNetRegioneService>();
@@ -27,18 +33,23 @@ namespace KampusStudioProto
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseStaticFiles();
-
+            app.UseRouting();
+            app.UseEndpoints(routeBuider => {
+                routeBuider.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+            /*
             app.UseMvc(routeBuilder =>
             {
                 routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+            */
         }
     }
 }
