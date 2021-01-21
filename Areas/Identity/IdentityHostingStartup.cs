d5ed1d581671b2d762311ae68ba63a2b68a2c126
Vincenzo Camporeale <vincenzo.camporeale@gmail.com>
2021-01-21 21:49:11 +0100

    Scaffolding di tutte le razor pages di Identity per predisporre il sistema di autenticazione alla traduzione in italiano

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using KampusStudioProto.Models.Services.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(KampusStudioProto.Areas.Identity.IdentityHostingStartup))]
+namespace KampusStudioProto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
