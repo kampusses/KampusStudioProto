bbbf2b8e7a85f862db69bd4bf003ddf06f27b3fa
Vincenzo Camporeale <vincenzo.camporeale@gmail.com>
2021-03-17 16:53:04 +0100

    Corretto bug inizializzazione azienda concessionaria (dava errore quando città non era ancora impostata)

--- Models/Services/Application/AdoNetComuneService.cs
+++ Models/Services/Application/AdoNetComuneService.cs
@@ -95,39 +95,45 @@ namespace KampusStudioProto.Models.Services.Application
 
         public async Task<ComuneViewModel> GetCodiceCatastaleComuneAsync(string codiceCatastaleComune)
         {
-            FormattableString query = $"SELECT * FROM comuni WHERE codiceCatastale={codiceCatastaleComune}";
-            DataSet dataSet = await db.QueryAsync(query);
-            var comuneTable = dataSet.Tables[0];
-            if (comuneTable.Rows.Count != 1)
+            if (codiceCatastaleComune != null && codiceCatastaleComune != "")
             {
-                throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {codiceCatastaleComune}");
-            }
-            var comuneRow = comuneTable.Rows[0];
-            var comuneViewModel = ComuneViewModel.FromDataRow(comuneRow);
+                FormattableString query = $"SELECT * FROM comuni WHERE codiceCatastale={codiceCatastaleComune}";
+                DataSet dataSet = await db.QueryAsync(query);
+                var comuneTable = dataSet.Tables[0];
+                if (comuneTable.Rows.Count != 1)
+                {
+                    throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {codiceCatastaleComune}");
+                }
+                var comuneRow = comuneTable.Rows[0];
+                var comuneViewModel = ComuneViewModel.FromDataRow(comuneRow);
 
-            FormattableString queryReg = $"SELECT * FROM regioni WHERE codiceRegione={comuneRow["codiceRegione"]}";
-            DataSet dataSetReg = await db.QueryAsync(queryReg);
-            var regioneTable = dataSetReg.Tables[0];
-            if (regioneTable.Rows.Count != 1)
-            {
-                throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {comuneRow["regione"]}");
-            }
-            var regioneRow = regioneTable.Rows[0];
-            var regioneViewModel = RegioneViewModel.FromDataRow(regioneRow);
-            comuneViewModel.Regione = (RegioneViewModel) regioneViewModel;
+                FormattableString queryReg = $"SELECT * FROM regioni WHERE codiceRegione={comuneRow["codiceRegione"]}";
+                DataSet dataSetReg = await db.QueryAsync(queryReg);
+                var regioneTable = dataSetReg.Tables[0];
+                if (regioneTable.Rows.Count != 1)
+                {
+                    throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {comuneRow["regione"]}");
+                }
+                var regioneRow = regioneTable.Rows[0];
+                var regioneViewModel = RegioneViewModel.FromDataRow(regioneRow);
+                comuneViewModel.Regione = (RegioneViewModel) regioneViewModel;
 
-            FormattableString queryPro = $"SELECT * FROM province WHERE codiceProvincia={comuneRow["codiceProvincia"]}";
-            DataSet dataSetPro = await db.QueryAsync(queryPro);
-            var provinciaTable = dataSetPro.Tables[0];
-            if (provinciaTable.Rows.Count != 1)
-            {
-                throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {comuneRow["provincia"]}");
+                FormattableString queryPro = $"SELECT * FROM province WHERE codiceProvincia={comuneRow["codiceProvincia"]}";
+                DataSet dataSetPro = await db.QueryAsync(queryPro);
+                var provinciaTable = dataSetPro.Tables[0];
+                if (provinciaTable.Rows.Count != 1)
+                {
+                    throw new InvalidOperationException($"Mi aspettavo che venisse restituita solo una riga della tabella {comuneRow["provincia"]}");
+                }
+                var provinciaRow = provinciaTable.Rows[0];
+                var provinciaViewModel = ProvinciaViewModel.FromDataRow(provinciaRow);
+                comuneViewModel.Provincia = (ProvinciaViewModel) provinciaViewModel;
+                return comuneViewModel;
             }
-            var provinciaRow = provinciaTable.Rows[0];
-            var provinciaViewModel = ProvinciaViewModel.FromDataRow(provinciaRow);
-            comuneViewModel.Provincia = (ProvinciaViewModel) provinciaViewModel;
-
-            return comuneViewModel;
+            else {
+                ComuneViewModel comuneViewModel = new ComuneViewModel();
+                return comuneViewModel;
+            }
         }
 
         public async Task<ListViewModel<ComuneViewModel>> GetComuniAsync(ComuneListInputModel model)
