5c205a8c8d09494b2de7fd04a1a84796c95e4313
Vincenzo Camporeale <vincenzo.camporeale@gmail.com>
2021-04-06 21:30:27 +0200

    Creata struttura MVC (controller + view) per la gestione utenti

---
+++ Controllers/UtentiController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KampusStudioProto.Models.InputModels;
+using KampusStudioProto.Models.Services.Application;
+using KampusStudioProto.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KampusStudioProto.Controllers
+{
+    public class UtentiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
